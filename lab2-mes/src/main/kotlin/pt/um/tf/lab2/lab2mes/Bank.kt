6e6c248f3a981f1adceaf093c1ed5a97d6a636e4
André Diogo <redan.blue27@gmail.com>
2018-04-23 17:58:50 +0100

    Changes Bank API to use Long for account balance.

--- lab2-mes/src/main/kotlin/pt/um/tf/lab2/lab2mes/Bank.kt
+++ lab2-mes/src/main/kotlin/pt/um/tf/lab2/lab2mes/Bank.kt
@@ -1,6 +1,6 @@
 package pt.um.tf.lab2.lab2mes
 
 interface Bank {
-    fun movement(mov : Int) : Boolean
-    fun balance() : Int
+    fun movement(mov : Long) : Boolean
+    fun balance() : Long
 }
--- lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Account.kt
+++ lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Account.kt
@@ -3,13 +3,13 @@ package pt.um.tf.lab2.lab2srv
 import pt.um.tf.lab2.lab2mes.Bank
 
 class Account : Bank {
-    override fun balance(): Int {
+    override fun balance(): Long {
         return balance
     }
 
-    private var balance : Int = 0
+    private var balance : Long = 0
 
-    override fun movement(mov : Int): Boolean {
+    override fun movement(mov : Long): Boolean {
         var res = true
         if (mov > 0) {
             balance += mov
