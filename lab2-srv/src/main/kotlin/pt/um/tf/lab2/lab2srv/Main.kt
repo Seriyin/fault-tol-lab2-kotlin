c2a6f4fdf6cd5c2311a22a8d8029ce1a88821370
André Diogo <redan.blue27@gmail.com>
2018-04-23 18:01:49 +0100

    Main restructured to handle different messages based on current phase.

--- lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Main.kt
+++ lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Main.kt
@@ -2,69 +2,135 @@ package pt.um.tf.lab2.lab2srv
 
 import io.atomix.catalyst.concurrent.SingleThreadContext
 import io.atomix.catalyst.serializer.Serializer
-import io.atomix.catalyst.transport.Address
-import io.atomix.catalyst.transport.Transport
-import io.atomix.catalyst.transport.netty.NettyTransport
 import pt.haslab.ekit.Spread
 import pt.um.tf.lab2.lab2mes.Message
 import pt.um.tf.lab2.lab2mes.NewMessage
 import pt.um.tf.lab2.lab2mes.Reply
+import pt.um.tf.lab2.lab2mes.UpdateMessage
+import spread.SpreadGroup
 import spread.SpreadMessage
 import java.util.*
 import java.util.concurrent.ThreadLocalRandom
+import java.util.logging.Level
+import java.util.logging.Logger
 
 
 fun main(args : Array<String>) {
-    val main = Main()
+    val main = Main(if (args.size>=0) args[0].toBoolean() else false)
     main.run()
 }
 
-class Main {
-    val me = Address("127.0.0.1", 22556)
-    val t : Transport = NettyTransport()
+class Main(s: Boolean) {
+    companion object {
+        val LOGGER : Logger = Logger.getLogger("Main")
+        val INIT_SPACE : Int = 2048
+    }
+
+    val tlr = ThreadLocalRandom.current()
+    val me = UUID(tlr.nextLong(), tlr.nextLong())
     val sr = Serializer()
     val tc = SingleThreadContext("srv-%d",sr)
     val acc = Account()
-    val tlr = ThreadLocalRandom.current()
-    val spread = Spread(me,
-            "srv-${UUID(tlr.nextLong(),
-                        tlr.nextLong())}", false)
-    var phase: Phase = Phase.initPhase()
+    val spread = Spread("srv-$me", false)
+    var phase: Phase = if (s) Phase.first() else Phase.initPhase()
+    val waitQueue : Queue<Message> by lazy {
+        ArrayDeque<Message>(INIT_SPACE)
+    }
+    lateinit var spreadGroup : SpreadGroup
 
     fun run() {
         sr.register(Message::class.java)
         sr.register(Reply::class.java)
         tc.execute(this::handlers)
+          .thenAccept(this::openAndJoin)
         while(readLine() == null);
+        spread.leave(spreadGroup)
+        spread.close()
         tc.close()
-        t.close()
-        println("I'm here")
+        LOGGER.log(Level.INFO,"I'm here")
+    }
+
+    private fun openAndJoin(void : Void) {
+        spread.open()
+        spreadGroup = spread.join("banks")
     }
 
     private fun handlers() {
-        println("Handling connection")
+        LOGGER.log(Level.INFO,"Handling connection")
         spread.handler(Message::class.java, {
             sm : SpreadMessage,
-            m : Message -> handler(m)
+            m : Message -> handler(sm, m)
         }).handler(Reply::class.java, {
             sm : SpreadMessage,
             m : Reply -> handler(m)
         }).handler(NewMessage::class.java, {
             sm : SpreadMessage,
-            m : NewMessage -> handler(m)
+            m : NewMessage -> handler(sm, m)
+        }).handler(UpdateMessage::class.java, {
+            sm : SpreadMessage,
+            m : UpdateMessage -> handler(m)
         })
     }
 
     private fun handler(m: Reply) {
+        LOGGER.log(Level.SEVERE, "Reply : $m")
+    }
+
+    private fun handler(sm: SpreadMessage,
+                        m: NewMessage) {
+        when (phase) {
+            Phase.UP -> {
+                val spm = SpreadMessage()
+                spm.addGroup(sm.sender)
+                spm.setCausal()
+                spread.multicast(spm, UpdateMessage(acc.balance()))
+            }
+            Phase.UNK -> {
+                if (m.origin == me)
+                    phase.change()
+            }
+            Phase.WAIT -> {}
+        }
 
     }
 
-    private fun handler(m: NewMessage) {
-
+    private fun handler(m: UpdateMessage) {
+        when (phase) {
+            Phase.WAIT -> {
+                acc.movement(m.accbalance)
+                for (item in waitQueue) {
+                    if(item.op == 1) {
+                        acc.movement(item.mov)
+                    }
+                }
+                waitQueue.clear()
+                phase.change()
+            }
+            Phase.UP -> {}
+            Phase.UNK -> {}
+        }
     }
 
-    private fun handler(m: Message) {
-
+    private fun handler(sm : SpreadMessage,
+                        m : Message) {
+        when (phase) {
+            Phase.WAIT -> {
+                waitQueue.add(m)
+            }
+            Phase.UP -> {
+                val spm = SpreadMessage()
+                spm.addGroup(sm.sender)
+                when (m.op) {
+                    1 -> {
+                        spread.multicast(spm, Reply(m.seq,1, acc.movement(m.mov), acc.balance()))
+                    }
+                    0 -> {
+                        spread.multicast(spm, Reply(m.seq,0, false, acc.balance()))
+                    }
+                }
+            }
+            Phase.UNK -> {}
+        }
     }
 
 }
