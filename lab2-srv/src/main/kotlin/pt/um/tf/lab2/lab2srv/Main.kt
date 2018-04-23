09d15305850240b1834fc2c55cd89291d8a4b560
André Diogo <redan.blue27@gmail.com>
2018-04-23 15:07:10 +0100

    Use spread wrapper + Phase enum for three phases of join.

--- lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Main.kt
+++ lab2-srv/src/main/kotlin/pt/um/tf/lab2/lab2srv/Main.kt
@@ -1,15 +1,17 @@
 package pt.um.tf.lab2.lab2srv
 
-import io.atomix.catalyst.concurrent.Futures
 import io.atomix.catalyst.concurrent.SingleThreadContext
 import io.atomix.catalyst.serializer.Serializer
 import io.atomix.catalyst.transport.Address
-import io.atomix.catalyst.transport.Connection
 import io.atomix.catalyst.transport.Transport
 import io.atomix.catalyst.transport.netty.NettyTransport
+import pt.haslab.ekit.Spread
 import pt.um.tf.lab2.lab2mes.Message
+import pt.um.tf.lab2.lab2mes.NewMessage
 import pt.um.tf.lab2.lab2mes.Reply
-import java.util.concurrent.CompletableFuture
+import spread.SpreadMessage
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
 
 
 fun main(args : Array<String>) {
@@ -23,30 +25,48 @@ class Main {
     val sr = Serializer()
     val tc = SingleThreadContext("srv-%d",sr)
     val acc = Account()
+    val tlr = ThreadLocalRandom.current()
+    val spread = Spread(me,
+            "srv-${UUID(tlr.nextLong(),
+                        tlr.nextLong())}", false)
+    var phase: Phase = Phase.initPhase()
 
     fun run() {
         sr.register(Message::class.java)
         sr.register(Reply::class.java)
-        tc.execute({t.server().listen(me, this::handlers)})
+        tc.execute(this::handlers)
         while(readLine() == null);
         tc.close()
         t.close()
         println("I'm here")
     }
 
-    private fun handlers(connection: Connection) {
+    private fun handlers() {
         println("Handling connection")
-        connection.handler(Message::class.java, this::handleOp)
+        spread.handler(Message::class.java, {
+            sm : SpreadMessage,
+            m : Message -> handler(m)
+        }).handler(Reply::class.java, {
+            sm : SpreadMessage,
+            m : Reply -> handler(m)
+        }).handler(NewMessage::class.java, {
+            sm : SpreadMessage,
+            m : NewMessage -> handler(m)
+        })
     }
 
-    private fun handleOp(it : Message) : CompletableFuture<Reply> {
-        println("Handle Message")
-        return when (it.op) {
-            0 -> Futures.completedFuture(Reply(it.op, true, acc.balance()))
-            1 -> Futures.completedFuture((Reply(it.op, acc.movement(it.mov), it.mov)))
-            else -> Futures.exceptionalFuture(IllegalArgumentException("Not recognized Message"))
-        }
+    private fun handler(m: Reply) {
+
     }
+
+    private fun handler(m: NewMessage) {
+
+    }
+
+    private fun handler(m: Message) {
+
+    }
+
 }
 
 
