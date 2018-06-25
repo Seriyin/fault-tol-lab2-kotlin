3ef98c925f3b42a9362edd23e12ec5f432b0d60f
André Diogo <redan.blue27@gmail.com>
2018-06-25 02:51:04 +0100

    Fixes spread operations not in catalyst context + spread message construction.

--- lab2-cli/src/main/kotlin/pt/um/tf/lab2/lab2cli/BankStub.kt
+++ lab2-cli/src/main/kotlin/pt/um/tf/lab2/lab2cli/BankStub.kt
@@ -2,41 +2,39 @@ package pt.um.tf.lab2.lab2cli
 
 import io.atomix.catalyst.concurrent.ThreadContext
 import io.atomix.catalyst.serializer.Serializer
+import mu.KLogging
 import pt.haslab.ekit.Spread
 import pt.um.tf.lab2.lab2mes.Bank
 import pt.um.tf.lab2.lab2mes.Message
 import pt.um.tf.lab2.lab2mes.Reply
 import spread.SpreadMessage
-import java.util.*
 import java.util.concurrent.CompletableFuture
-import java.util.logging.Level
-import java.util.logging.Logger
 
-class BankStub(val me: UUID,
+class BankStub(val me: String,
                val sp: Spread,
-               val sr: Serializer,
                val tc: ThreadContext) : Bank
 {
-    companion object {
-        val LOGGER : Logger = Logger.getLogger("BankStub")
-    }
+    companion object : KLogging()
 
-    private var comFMov : CompletableFuture<Boolean> = CompletableFuture()
-    private var comFBal : CompletableFuture<Long> = CompletableFuture()
+    private lateinit var comFMov : CompletableFuture<Boolean>
+    private lateinit var comFBal : CompletableFuture<Long>
     private var i : Int = 0
 
     init {
-        sp.open()
-        sp.handler(Reply::class.java, {
-            sm: SpreadMessage,
-            m: Reply -> handle(m)
-        })
+        tc.execute {
+            sp.open()
+            sp.join("banks")
+            sp.handler(Reply::class.java, this@BankStub::handle)
+        }.get()
     }
 
     override fun movement(mov: Long): Boolean {
         comFMov = CompletableFuture()
         tc.execute {
-            sp.multicast(SpreadMessage(), Message(i,1, mov, me))
+            val spm = SpreadMessage()
+            spm.setSafe()
+            spm.addGroup("banks")
+            sp.multicast(spm, Message(i,1, mov, me))
         }
         return comFMov.get()
     }
@@ -44,24 +42,29 @@ class BankStub(val me: UUID,
     override fun balance(): Long {
         comFBal = CompletableFuture()
         tc.execute {
-            sp.multicast(SpreadMessage(), Message(i,0, 0, me))
+            val spm = SpreadMessage()
+            spm.setSafe()
+            spm.addGroup("banks")
+            sp.multicast(spm, Message(i,0, 0, me))
         }
         return comFBal.get()
     }
 
 
-    private fun handle(m: Reply) {
+    private fun handle(sm : SpreadMessage, m: Reply) {
+        logger.info { "Register reply" }
         when (m.op) {
             1 -> when {
-                m.seq < i -> LOGGER.log(Level.INFO,"Repeat message")
+                m.seq < i -> logger.info { "Repeat message" }
                 m.seq == i -> {
                     comFMov.complete(m.denied)
                     i++
                 }
-                else -> LOGGER.log(Level.SEVERE, "Sequence is ahead of messaging")
+                else -> logger.error { "Sequence is ahead of messaging" }
             }
             0 -> comFBal.complete(m.balance)
         }
     }
+
 
 }
